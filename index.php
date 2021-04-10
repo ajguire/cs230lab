f3fbaf9479de7ce792c2b632a49edfd5ce4b2e4a
ajguire <ajguire2@gmail.com>
2021-04-10 18:56:08 -0400

    Minor Fixes

--- index.php
+++ index.php
@@ -2,12 +2,14 @@
 require 'includes/header.php'
 ?>
 
-<main> 
-    <link rel="stylesheet" href="css/about.css"> 
+<main>
+    <link rel="stylesheet" href="css/about.css">
 
     <div class="about">
         <h1> About us </h1>
-        <p> Our team is composed of a world renowned docter, two popular and talented racing drivers, and a clown that has failed upwards to a position he is unsuited for. Together, they run this website that sells TDB at competative prices.</p>
+        <p> Our team is composed of a world renowned docter, two popular and talented racing drivers, and a clown that
+            has failed upwards to a position he is unsuited for. Together, they run this website that sells TDB at
+            competative prices.</p>
     </div>
     <div class="row">
         <div class="column">
@@ -21,41 +23,37 @@ require 'includes/header.php'
             </div>
         </div>
 
-        <div class="row">
-            <div class="column">
-                <div class="card">
-                    <img src="images/thestig.jpeg">
-                    <div class="container">
-                        <h2> COO </h2>
-                        <p>The Stig</p>
-                        <p>Email: none@genericemail</p>
-                    </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/bobkelsomd.jpeg">
+                <div class="container">
+                    <h2> CEO </h2>
+                    <p>Bob Kelso, MD</p>
+                    <p>Email: bobkelsomd@genericemail</p>
                 </div>
             </div>
-        
-            <div class="row">
-                <div class="column">
-                    <div class="card">
-                        <img src="images/sainz.jpeg">
-                        <div class="container">
-                            <h2> CTO </h2>
-                            <p>Carlos Sainz</p>
-                            <p>Email: CarlosSainz55@genericeamil</p>
-                        </div>
-                    </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/bobkelsomd.jpeg">
+                <div class="container">
+                    <h2> CEO </h2>
+                    <p>Bob Kelso, MD</p>
+                    <p>Email: bobkelsomd@genericemail</p>
                 </div>
-        
-                <div class="row">
-                    <div class="column">
-                        <div class="card">
-                            <img src="images/clown.jpeg">
-                            <div class="container">
-                                <h2> A Massive Clown</h2>
-                                <p>Mattia Binotto</p>
-                                <p>Email: mbinotto@genericemail</p>
-                            </div>
-                        </div>
-                    </div>
-                    
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/bobkelsomd.jpeg">
+                <div class="container">
+                    <h2> CEO </h2>
+                    <p>Bob Kelso, MD</p>
+                    <p>Email: bobkelsomd@genericemail</p>
+                </div>
+            </div>
+        </div>
     </div>
 </main>
